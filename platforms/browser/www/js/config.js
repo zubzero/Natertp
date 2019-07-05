b09a78cde0567a774baeb04acc73f6b7d62a7bc1
Rafael Robles <gdl.villber@gmail.com>
2019-07-05 11:43:51 -0500

    CAmbios conmpletos, pero paypal no funciona.

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,34 @@
+angular.module("woocommerce-api").constant("CONFIG", {
+
+    // The url of your domain, both HTTP and HTTPS are supported.
+    site_url: 'https://naturapp.store',
+    // site_url: 'http://localhost:8888/mobile/woocommerce-api/wordpress',
+
+    // Max period of time to wait for reply from the server, defined in milliseconds.
+    request_timeout: 6000,
+
+    // The url that follows your main domain, the API version is of interest here, v3 is the latest.
+    wc_api_endpoint: '/wc-api/v3',
+
+    // Signature method
+    oauth_signature_method: 'HMAC-SHA256',
+    // oauth_signature_method: 'HMAC-SHA1',
+
+    // Pair of credentials from your woocommerce installation, please refer to the documentation.
+    // Apps today
+    wc_consumer_key: 'ck_4f75656f7008000c0f7c3f5974ac686319547da1',
+    wc_consumer_secret: 'cs_9d2ddeb675e8940526a844537fa0d872045c72c0',
+    // Localhost
+    // wc_consumer_key: 'ck_86a13f2c0ec34a4c99374e120dba436d31f069c3',
+    // wc_consumer_secret: 'cs_a0bc69e05eced4a8f464c08849c8f543946448d9',
+
+    // The number of products to be fetched with each API call.
+    products_per_page: 6,
+
+    // The number of reviews to be fetched with each API call.
+    reviews_per_page: 6,
+
+    // Frontpage Slideshow Category
+    slideshow_category: 'alimentos'
+
+})
